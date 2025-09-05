8c79a16856a067216dc5ab8650e85bb38a933cc2
Sam <samueladeleke392@gmail.com>
2025-09-05 09:25:30 +0100

    Made some changes to the admin dashboard

--- src/app/pages/admin/layout/layout.ts
+++ src/app/pages/admin/layout/layout.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-layout',
@@ -8,12 +8,25 @@ import { Component } from '@angular/core';
 })
 export class Layout {
   collapsed: boolean = false;
+  isMobile: boolean = false;
 
   ngOnInit() {
-    this.collapsed = window.innerWidth <= 768;
+    this.checkScreenSize(); 
+  }
+
+  @HostListener('window:resize')
+  checkScreenSize() {
+    this.isMobile = window.innerWidth <= 768;
+    this.collapsed = this.isMobile ? true : false; 
   }
 
   toggleSidebar() {
     this.collapsed = !this.collapsed
+  }
+
+  onNavClick() {
+    if(this.isMobile) {
+      this.collapsed = true;
+    }
   }
 }
